a77bacf3cb9b2e60cb325ffd84eab85724ad9a74
DADIENG ALFRED GABRIEL <dadiengalfred18@gmail.com>
2026-01-16 01:24:07 +0100

    updated

--- app/app/components/admin/AdminSidebar.tsx
+++ app/app/components/admin/AdminSidebar.tsx
@@ -79,7 +79,7 @@ export function AdminSidebar() {
                                 <div className="h-full bg-purple-500 w-full animate-pulse"></div>
                             </div>
                             <button
-                                onClick={() => signOut({ callbackUrl: "/" })}
+                                onClick={() => signOut({ callbackUrl: "https://www.africaresearchbase.com/" })}
                                 className="flex w-full items-center justify-center gap-2 rounded-lg bg-white/10 hover:bg-white/20 py-2 text-xs font-semibold text-white transition-all"
                             >
                                 <ArrowRightOnRectangleIcon className="h-4 w-4" />
